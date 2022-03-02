d424916bce303a5ec92981f7795dad27c77b1dd4
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-03-01 23:13:41 -0500

    initial redux files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
 
+//STORE -> Globalized state
+
+//ACTION -> Different actions you want to do
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+//REDUCER -> checks action and based on action modifies store
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      break;
+  }
+};
+
+let store = createStore(counter);
+
+//Display in console
+store.subscribe(() => console.log(store.getState()));
+//DISPATCH -> executes action
+store.dispatch(increment());
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
